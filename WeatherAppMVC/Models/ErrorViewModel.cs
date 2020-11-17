594acbbfe9c8893bc27844343e08c539a452891f
Nikolai Sison <sisonnikolai@gmail.com>
2020-11-17 11:09:52 +0800

    Added the main project

---
+++ WeatherAppMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WeatherAppMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
